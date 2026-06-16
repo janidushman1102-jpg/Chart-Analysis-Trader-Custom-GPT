f2e13356453251ed8a20583bd790fdea8673c4eb
janidushman1102-jpg <janidushman1102@gmail.com>
2026-06-16 15:29:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Chart Analysis Trader API",
+    version="1.0.0",
+    servers=[
+        {
+            "url": https://chart-analysis-trader-custom-gpt-production.up.railway.app/openapi.json
+        }
+    ]
+)
 
 @app.get("/")
 def root():
-    return {"message": "Hello from Koyeb"}
+    return {"status": "running"}
